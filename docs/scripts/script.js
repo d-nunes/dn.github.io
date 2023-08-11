4ab842eb606ffa37d1f525af5e47127b9e3c6022
d-nunes <71280611+d-nunes@users.noreply.github.com>
2023-08-11 11:40:20 -0700

    added all files

---
+++ docs/scripts/script.js
@@ -0,0 +1,87 @@
+function HomepageLinks(url)
+{
+    //find the element with the class "headerBar" and manipulate its width
+    document.querySelector(".headerBar").setAttribute("style", "width:100vw");
+
+    setTimeout
+    (
+        function() 
+        {
+            document.querySelector(".screen").setAttribute("style", "height: 100vh");
+            document.querySelector(".homeButton").setAttribute("style", "opacity: 1");
+        }, 500
+    );
+
+    setTimeout
+    (
+        function()
+        {
+            window.open(url, "_self");
+        }, 1250
+    );
+}
+
+function NonHomepageLinks(url)
+{
+    document.querySelector(".screen").setAttribute("style", "height: 100vh");
+    
+    setTimeout
+    (
+        function()
+        {
+            window.open(url, "_self");
+        }, 750
+    );
+}
+
+function OnHomeButtonClick()
+{
+    document.querySelector(".homeButton").setAttribute("style", "opacity: 0");
+    
+    document.querySelector(".screen").setAttribute("style", "height: 100vh");
+    
+    setTimeout
+    (
+        function() 
+        {
+            document.querySelector(".headerBar").setAttribute("style", "width: 0vw");
+        }, 500
+    );
+
+    setTimeout
+    (
+        function()
+        {
+            window.open("../index.html", "_self");
+        }, 750
+    );
+}
+
+function PullUpScreen()
+{
+    document.querySelector(".screen").setAttribute("style", "height: 0vh");
+}
+
+
+function OpenNewWindow(url)
+{
+    document.querySelector(".screen").setAttribute("style", "height: 100vh");
+    
+    setTimeout
+    (
+        function()
+        {
+            window.open(url, "_self");
+        }, 500
+    );
+}
+
+function OpenNewTab(url)
+{
+    window.open(url);
+}
+
+function Grow(id)
+{
+    document.querySelector("#" + id).setAttribute("style", "height: 100vh");
+}
